4ffd5ab597c31785429e4423dadc5f758360403a
reidsvilleneuve <reidsvilleneuve@gmail.com>
2014-11-26 17:32:12 -0700

    Enabled physics / movement
    TODO: Set bounds for top, left, and right.

---
+++ BouncyBall/BouncyBall/GameForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace BouncyBall
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 1;
+            this.GameTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Ball
+            // 
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(264, 224);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(50, 50);
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(569, 464);
+            this.Controls.Add(this.Ball);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Location = new System.Drawing.Point(50, 50);
+            this.MaximizeBox = false;
+            this.Name = "GameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Bouncy Ball!";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.Move += new System.EventHandler(this.GameForm_Move);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox Ball;
+    }
+}
+
---
+++ BouncyBall/BouncyBall/GameForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncyBall
+{
+    public partial class GameForm : Form
+    {
+        int newLocationX = 0;
+        int newLocationY = 0;
+
+        int BallXMomentum = 0;
+        int BallYMomentum = 0;
+
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GameForm_Move(object sender, EventArgs e)
+        {
+            if (newLocationX != this.Location.X)
+                Ball.Left -= this.Location.X - newLocationX;
+
+            if (newLocationY != this.Location.Y)
+                Ball.Top -= this.Location.Y - newLocationY;
+
+            if (Ball.Top < 0)
+                Ball.Top = 0;
+
+            if (Ball.Left < 0)
+            {
+                //BallXMomentum = 0 - 
+                Ball.Left = 0;
+            }
+
+            if (Ball.Left > (this.Width - (int)(Ball.Width * 1.32)))
+                Ball.Left = this.Width - (int)(Ball.Width * 1.32);
+            
+            if (Ball.Top > (this.Height - (int)(Ball.Height * 1.78)))
+            {
+
+                BallYMomentum += this.Location.Y - newLocationY;
+                Ball.Top =(this.Height - (int)(Ball.Height * 1.78));
+            }
+  
+            newLocationX = this.Location.X;
+            newLocationY = this.Location.Y;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BallYMomentum++;
+            Ball.Top += BallYMomentum;
+
+            if (!(Ball.Top < this.Height - (int)(Ball.Height * 1.78)))
+            {
+                    BallYMomentum = (int)(BallYMomentum / 1.5);
+                    BallYMomentum = -BallYMomentum;
+            }
+
+            if (!(Ball.Top < this.Height - (int)(Ball.Height * 1.78)))
+            {
+                Ball.Top = this.Height - (int)(Ball.Height * 1.78);
+            }
+           
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            newLocationX = this.Location.X;
+            newLocationY = this.Location.Y;
+        }
+
+        private void GameForm_Move()
+        {
+
+        }
+    }
+}
